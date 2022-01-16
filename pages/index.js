1c5b0108be6f05fa91d0ce0bf8e806b2d32eb42c
paulweezydesign <weezy@paulweezydesign.com>
2022-01-16 04:37:20 -0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div>
       <Head>
+      <script src="https://cdn.tailwindcss.com"></script>
+ <link href="https://cdn.jsdelivr.net/npm/daisyui@1.23.0/dist/full.css" rel="stylesheet" type="text/css" />
         <title>This page has a title 🤔</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
